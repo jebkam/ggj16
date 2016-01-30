903c97077fd1da2f506880b85f9a16370b17d218
jebkam <jebkam@gmail.com>
2016-01-30 03:16:45 -0500

    First push, project files with fsm working with 'Return' to go to PlayGame and 'Q' to go back to MainMenu.

---
+++ Assets/Scripts/GameModeFSM.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameModeFSM : MonoBehaviour 
+{
+	private SimpleTouchAreaButton touchPanel;
+	private GJM_StartOptions startOptions;
+
+	[SerializeField] public enum GameStates
+	{
+		MainMenu,
+		PlayGame
+	}
+
+	[SerializeField] public GameStates currentState;
+
+	Dictionary <GameStates, Action> fsm = new Dictionary <GameStates, Action> ();
+
+	public void SetState (GameStates nextState)
+	{
+		if (nextState != currentState)
+		{
+			currentState = nextState;
+			Debug.Log ("Switching to " + nextState);
+		}
+	}
+		
+	// Use this for initialization
+	void Start () 
+	{
+		touchPanel = GameObject.Find("TouchPanel").GetComponent<SimpleTouchAreaButton>();
+
+		fsm.Add (GameStates.MainMenu, MainMenuState);
+		fsm.Add (GameStates.PlayGame, PlayGameState);
+
+		SetState (GameStates.MainMenu);
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+		if (fsm[currentState] != null)
+		{
+			fsm[currentState].Invoke();
+		}
+
+	}
+		 
+////////////////////////////////
+/// <summary>
+/// 
+/// Main Menu Stuff Goes Here
+/// 
+/// </summary>
+///////////////////////////////
+
+	public void MainMenuState ()
+	{
+		if (Input.GetKeyDown(KeyCode.Return))
+		{
+			SetState(GameStates.PlayGame);
+		}
+	}
+
+///////////////////////////////////
+/// <summary>
+/// 
+/// Main Gameplay Stuff Goes Here
+/// 
+/// </summary>
+//////////////////////////////////
+
+	public void PlayGameState ()
+	{
+		if (Input.GetKey(KeyCode.Q))
+		{
+			SetState(GameStates.MainMenu);
+		}
+	}
+
+
+
+}
---
+++ Assets/Scripts/SimpleTouchAreaButton.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+public class SimpleTouchAreaButton : MonoBehaviour, IPointerDownHandler, IPointerUpHandler {
+	
+	private bool touched;
+	private int pointerID;
+	private bool canAccessButtonArea; 
+	
+	void Awake () {
+		touched = false;
+		canAccessButtonArea = false;
+	}
+	
+	public void OnPointerDown (PointerEventData data) {
+		if (!touched) {
+			touched = true;
+			pointerID = data.pointerId;
+			canAccessButtonArea = true;
+		}
+	}
+	
+	public void OnPointerUp (PointerEventData data) {
+		if (data.pointerId == pointerID) {
+			canAccessButtonArea = false;
+			touched = false;
+		}
+	}
+	
+	public bool CanAccessButtonArea () {
+		return canAccessButtonArea;
+	}
+	
+}
